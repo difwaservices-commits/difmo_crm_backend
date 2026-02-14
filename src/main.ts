d19aa4e84b89a978deb8890d770fde7eec36b917
Rajesh <rajesh@Rajeshs-MacBook-Air.local>
2026-02-14 22:34:57 +0530

    save

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { TransformInterceptor } from './common/interceptors/transform.interceptor';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as yaml from 'yaml';
-import * as fs from 'fs';
+import { setupApp } from './setup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors();
-
-  const config = new DocumentBuilder()
-    .setTitle('Difmo CRM API')
-    .setDescription('The Difmo CRM API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addServer('http://localhost:3000', 'Development')
-    .addServer('https://api.difmocrm.com', 'Production')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
-  const yamlString = yaml.stringify(document, {});
-  fs.writeFileSync('./swagger.yaml', yamlString);
-
-  SwaggerModule.setup('api', app, document);
-
+  setupApp(app);
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
