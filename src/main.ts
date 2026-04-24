dbbb3608b04cffff555228143d56b6a5cd280951
ramjeaimt <ramjeeaimt@gmail.com>
2026-04-24 12:39:11 +0530

    tsconfig9

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupApp } from './setup';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import * as dns from 'node:dns';
 
 async function bootstrap() {
+  // Force IPv4 for database connections and other network requests
+  // This fixes ENETUNREACH errors on platforms like Render/Vercel
+  dns.setDefaultResultOrder('ipv4first');
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TransformInterceptor());
